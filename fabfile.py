8beb5e02bf0825d38fe8d6e4292666aef256f2cf
Dan Carroll <dancarroll@gmail.com>
2013-03-01 01:02:52 -0500

    Add fabfile for local tasks like cleaning up deploy folder, hyde serve

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import *
+import os
+
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
+
+def clean():
+    local('rm -rf ./deploy')
+
+def generate():
+    local('hyde gen')
+
+def regen():
+    clean()
+    generate()
+
+def serve():
+    local('hyde serve')
+
+def reserve():
+    regen()
+    serve()
+
